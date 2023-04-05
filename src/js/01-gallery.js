e3c3a93ce6a3d50238a3b83b7dab39dca9e4783a
Viktoria Khmelevska <vikahvl@ukr.net>
2023-04-05 23:33:18 +0300

    task1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,36 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+
+const markup = createMarkup(galleryItems);
+
+container.insertAdjacentHTML('beforeend', markup);
+
+function createMarkup(items) { 
+    return items.map(({ preview, original, description }) => 
+         `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    ).join('');
+};
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    closeText: '×',
+    widthRatio: 0.8,
+    heightRatio: 0.8,
+});
