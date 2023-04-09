089e470d6cda5c7608064d1c4c6fc67ec270e116
Viktoria Khmelevska <vikahvl@ukr.net>
2023-04-09 23:38:25 +0300

    add task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+function secondTime (data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds))
+};
+
+    player.on('timeupdate', throttle(secondTime, 1000));
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
+       const secondStop = JSON.parse(localStorage.getItem(STORAGE_KEY));
+player.setCurrentTime(secondStop).then(function (seconds) {
+    if (!secondStop) {
+        return;
+    }
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
