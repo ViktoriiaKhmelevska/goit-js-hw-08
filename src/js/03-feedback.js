903d35f6d96e31e7b3335258f803b2264916de74
Viktoria Khmelevska <vikahvl@ukr.net>
2023-04-15 23:34:56 +0300

    dont undestand

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,21 +7,16 @@ const data = {
     email: document.querySelector('.js-feedback-form input'),
     message: document.querySelector('.js-feedback-form textarea'),
 }
-
 const formData = {};
 
 data.form.addEventListener('submit', onFormSubmit);
 data.form.addEventListener('input', throttle(onInput, 500));
 
 function onInput(e) {
-    // formData.email = "";
-    // formData.message = "";
-    if (e.target.value) {
+
+        if (e.target.value) {
         formData[e.target.name] = e.target.value;
       }
-    else {
-        formData[e.target.name] = "";
-    }
  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));     
 };
 
@@ -34,26 +29,24 @@ function onFormSubmit(e) {
     }
     e.preventDefault();
     e.target.reset();
+
     const res = JSON.parse(localStorage.getItem(STORAGE_KEY));
     console.log(res);
-
+//   const { email, message } = e.target.elements;
+//   const formData = {
+//     email : email.value,
+//     message: message.value
+//     }
+//     console.log(formData);
     localStorage.removeItem(STORAGE_KEY);
  };
 
 function populateTextarea() {
-    let savedMessage = (JSON.parse(localStorage.getItem(STORAGE_KEY)) || "")
-    if (savedMessage.email !== "") {
-    data.email.value = savedMessage.email;
+    let savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        // data.email.value = savedMessage.email;
+        //   data.message.value = savedMessage.message;
+    if (savedMessage) {
+         data.email.value = savedMessage.email || "";
+        data.message.value = savedMessage.message || "";
     }
-    if (savedMessage.message !== "") {
-         data.message.value = savedMessage.message;
-    }
-        
-    // if (!(savedMessage.email === "" || savedMessage.message === "")) {
-    //     data.email.value = savedMessage.email;
-    //     data.message.value = savedMessage.message;
-    // } else {
-    //         data.email.value = "";
-    // data.message.value = "";
-    // }
     };
