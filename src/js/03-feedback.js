d83618d8962e4c07509538429437149951c7b72d
Viktoria Khmelevska <vikahvl@ukr.net>
2023-04-16 20:10:04 +0300

    final corrections

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,7 @@ const data = {
     email: document.querySelector('.js-feedback-form input'),
     message: document.querySelector('.js-feedback-form textarea'),
 }
-const formData = {};
+let formData = {};
 
 data.form.addEventListener('submit', onFormSubmit);
 data.form.addEventListener('input', throttle(onInput, 500));
@@ -24,20 +24,15 @@ function onInput(e) {
 };
 
 function onFormSubmit(e) { 
+
     e.preventDefault();
     if ((data.email.value ==="" || data.message.value ==="")) {
         alert(`Необхідно заповнити всі поля форми`)
         return;
     };
 
-    const res = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    console.log(res);
-//   const { email, message } = e.target.elements;
-//   const formData = {
-//     email : email.value,
-//     message: message.value
-//     }
-//     console.log(formData);
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
     formData = {};
     data.form.reset();
     localStorage.removeItem(STORAGE_KEY);
